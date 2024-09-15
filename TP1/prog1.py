ce68f45a5612a86eda49b3b2c247bdbaa712674b
carmella19 <nguimbicarmella@gmail.com>
2024-09-15 12:51:58 -0400

    ajout de modifs

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,15 @@
+import fonctions as f
+
+
 print("hello, World!")
+
+while True : 
+
+    nombre = int(input("Entrez un nombre: "))
+    print("Le carré de ce nombre est:", nombre*nombre)
+
+    v1 = int(input("Entrer la premiere valeur: "))
+    v2 = int(input("Entrer la deuxième valeur: "))
+
+    res = f.puissance (v1,v2)
+    print (res)
